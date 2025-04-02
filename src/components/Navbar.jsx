3c757b5ce3fa7ea080e20650022be2499fff233c
Rakshi2609 <rakshithganjimut@gmail.com>
2025-04-02 10:57:04 +0530

    Adjust line endings

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,6 @@ const Navbar = () => {
             <span className='font-bold text-xl mx-8'>sTask</span>
         </div>
       <ul className="flex gap-8 mx-9">
-        <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
         <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
       </ul>
     </nav>
